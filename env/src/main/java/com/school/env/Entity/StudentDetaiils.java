ae72028eb1525d73960d4e23abe5270f77a71fb2
MGUPTA5 <mgupta5@visteon.com>
2026-03-16 20:09:37 +0530

    Project lombok for removing boilerplate code

--- env/src/main/java/com/school/env/Entity/StudentDetaiils.java
+++ env/src/main/java/com/school/env/Entity/StudentDetaiils.java
@@ -1,37 +1,16 @@
 package com.school.env.Entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Document(collection = "student_details")
+@Data
 public class StudentDetaiils {
 
     @Id
     String id;
     String name;
     String standard;
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStandard() {
-        return standard;
-    }
-
-    public void setStandard(String standard) {
-        this.standard = standard;
-    }
 }
